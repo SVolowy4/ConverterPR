4a643af6208971c4e40fe0a507ce45d4a0b7d3c7
SVolowy4 <121169529+SVolowy4@users.noreply.github.com>
2024-03-06 10:17:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let input = document.getElementById('input')
+
 let content = document.getElementById('content')
 
 
 
 function convert () {
    let count = input.value;
-   let tips = Math.round(parseInt(count) * (0.35/100));
-   content.textContent = tips + " uah"
+   let tips = (parseInt(count) * (0.35/100));
+   content.style.color ='#00acb59d'
+   content.style.fontSize = '3.5em'
+   content.style.fontWeight = 'bold'
+   content.style.backgroundColor = 'black'
+   content.textContent = tips.toFixed(0) + " на"
    return input.value = ''
 }
