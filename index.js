ebc26f9a33f3486328e6e999d591e434e7cc3cac
SVolowy4 <121169529+SVolowy4@users.noreply.github.com>
2025-02-09 21:18:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ const objects = [
 link: 'https://send.monobank.ua/9qLk4HnKWM' },
   { name: 'Кухня/Кондитери', procent: 5 / 100, link: 'https://send.monobank.ua/jar/9bBSCWEDDA' },
   { name: 'Ранер', procent: 2.6 / 100, 
-link: 'https://send.monobank.ua/6J3vq8Gun5'},
+link: 'https://send.monobank.ua/5wUkX8rFWo'},
   { name: 'Хостес', procent: 0.8 / 100, 
-link: 'https://send.monobank.ua/3XyV5gTj4f' }
+link: 'https://send.monobank.ua/6J3vq8Gun5' }
 ]
 
 
